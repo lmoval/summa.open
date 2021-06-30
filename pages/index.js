ad884472c42ff1b3d3d624580f0aff23ef580e50
Luca Morelli <luca.morelli@ovalmoney.com>
2021-06-30 11:25:26 +0200

    fix: lint: remove unused components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from '../comps/Navbar';
-import Footer from '../comps/Footer';
 import styles from '../styles/Home.module.css';
 import Search from '../comps/Search';
 
