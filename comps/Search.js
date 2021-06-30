deab27d7e02ce4a0bb06319ee2f78d3cd801d03a
Luca Morelli <luca.morelli@ovalmoney.com>
2021-06-30 11:24:59 +0200

    fix: add instasearch.css reset.css stylesheet

--- comps/Search.js
+++ comps/Search.js
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
-// import 'instantsearch.css/themes/reset.css';
+import 'instantsearch.css/themes/reset.css';
 
 const searchClient = algoliasearch('QDHDBDXY6L', '02a878859cf6834371adff3b0423a271');
 
