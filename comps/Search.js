6a0501aa158df363e167cac3957af97f3214ab86
Luca Morelli <luca.morelli@ovalmoney.com>
2021-06-30 11:24:37 +0200

    fix: remove CSS modul-ish instantsearch style and add it globally

--- comps/Search.js
+++ comps/Search.js
@@ -1,6 +1,5 @@
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
-// import styles from './Search.module.css';
 // import 'instantsearch.css/themes/reset.css';
 
 const searchClient = algoliasearch('QDHDBDXY6L', '02a878859cf6834371adff3b0423a271');
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../comps/Layout';
 import '../styles/globals.css';
 import { useState } from 'react';
 import Search from '../comps/Search';
+import '../styles/ais.css';
 
 function MyApp({ Component, pageProps }) {
   return (
